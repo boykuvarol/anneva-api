f0123a5a2566d37a6f28d7bba3be350be5886d45
buseoykuvarol <oeykue@percon.io>
2026-07-30 10:34:56 +0300

    fix: configure Prisma 7 PostgreSQL adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,20 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
 
-  async onModuleDestroy() {
-    await this.$disconnect();
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+
+    super({ adapter });
   }
 }
